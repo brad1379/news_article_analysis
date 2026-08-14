cecc64a9024e1ac23b3a14adb8f5481a28acf73f
Brad Arnold <brad1379@gmail.com>
2026-08-14 17:54:51 -0500

    first commit

---
+++ pythonAssessment.py
@@ -0,0 +1,19 @@
+def count_specific_word(str1, str2):
+    # Edge case: empty string should return 0
+    return
+
+def identify_most_common_word(str):
+    # Edge case: empty string should return None
+    return
+
+def calculate_average_word_length(str):
+    # Edge case: empty string should return 0
+    return
+
+def count_paragraphs(str):
+    # Edge case: empty string should return 1
+    return
+
+def count_sentences(str):
+    # Edge case: empty string should return 1
+    return
